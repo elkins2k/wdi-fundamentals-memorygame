076409b32fd943fe7196eb6b5509fde8b53762cd
elkins2k <elkins2k@mail.com>
2020-01-07 10:02:39 -0600

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,5 +1,18 @@
-console.log("Up and running!");
-let cardOne = "queen";
-let cardTwo = "king";
-let cardThree = "queen";
-let cardFour = "king";
+let cards = ['queen' ,'queen','king','king'];
+let cardsInPlay = [];
+let cardOne=cards[0];
+cardsInPlay.push(cardOne);
+console.log(cardsInPlay);
+
+let cardTwo=cards[2];
+cardsInPlay.push(cardTwo);
+console.log(cardsInPlay);
+
+if (cardsInPlay.length===2)
+	{
+		if (cardsInPlay[0]===cardsInPlay[1])
+		{alert("You found a match");
+		} else 
+		{alert("Sorry, try again.");
+		}
+	}
